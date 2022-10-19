94585a871e1c80587c19fb7b5f22c2fe82b8d60b
Allison <farrella95@gmail.com>
2022-10-19 17:20:49 -0400

    saliukghdasflkg

--- server.js
+++ server.js
@@ -1,18 +1,66 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
 
-inquirer
-  .prompt([
+// Node v10+ includes a promises module as an alternative to using callbacks with file system methods.
+const { writeFile } = require("fs").promises;
+
+// Use writeFileSync method to use promises instead of a callback function
+
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "name",
       message: "What is your name?",
     },
-  ])
-  .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    {
+      type: "input",
+      name: "location",
+      message: "Where are you from?",
+    },
+    {
+      type: "input",
+      name: "hobby",
+      message: "What is your favorite hobby?",
+    },
+    {
+      type: "input",
+      name: "food",
+      message: "What is your favorite food?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL.",
+    },
+  ]);
+};
 
-    fs.writeFile("index.html", htmlPageContent, (err) =>
-      err ? console.log(err) : console.log("Successfully created index.html!")
-    );
-  });
+const generateHTML = ({ name, location, github, linkedin }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+</div>
+</body>
+</html>`;
+
+const init = () => {
+  promptUser()
+    .then((answers) => writeFile("index.html", generateHTML(answers)))
+    .then(() => console.log("Successfully wrote to index.html"))
+    .catch((err) => console.error(err));
+};
+
+init();
